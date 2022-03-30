717dd9e441883a6025c359f554a92c187934bc80
Bernardo Andrade <bpires99@gmail.com>
2022-03-30 10:27:25 -0300

    Navigator

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,20 @@ import { ArtigoPage } from './pages/Artigo';
 import { MeusArtigosPage } from './pages/MeusArtigos';
 import { EditarArquivoPage } from './pages/EditarArquivo';
 import { NotFoundPage } from './pages/NotFound';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<ArtigosPage />} />
-        <Route path="/artigo/:id" element={<ArtigoPage />} />
-        <Route path="/artigos" element={<MeusArtigosPage />} />
-        <Route path="/artigos/editar/:id" element={<EditarArquivoPage />} />
-        <Route path="/artigos/novo" element={<EditarArquivoPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ArtigosPage />} />
+          <Route path="/artigo/:id" element={<ArtigoPage />} />
+          <Route path="/artigos" element={<MeusArtigosPage />} />
+          <Route path="/artigos/editar/:id" element={<EditarArquivoPage />} />
+          <Route path="/artigos/novo" element={<EditarArquivoPage />} />
+        </Route>
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
