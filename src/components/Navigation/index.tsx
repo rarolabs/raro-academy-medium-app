e2a52496e2db696e8ad35a82c3f5ab02577d9fb1
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-03-30 16:24:01 -0300

    Criação componente Navigation

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export const Navigation = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/artigos">Meus Artigos</Link>
+      <Link to="/artigos/novo">Novo Artigo</Link>
+    </>
+  );
+};
