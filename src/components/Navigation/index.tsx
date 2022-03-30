8d42ee52db47c6dcd9bccff96f685c84ec89e135
Bernardo Andrade <bpires99@gmail.com>
2022-03-30 11:02:42 -0300

    login and logout navigation buttons

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
+
+  const isLoggedIn = true
+
   return (
       <>
         <Link to="/">Home</Link>
         <Link to="/artigos">Meus Artigos</Link>
         <Link to="/artigos/novo">Novo Artigo</Link>
+        {!isLoggedIn && <Link to="/login">Login</Link>}
+        <Link to="/">Logout</Link>
       </>
   );
 };
