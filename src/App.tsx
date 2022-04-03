5b8a2a980998eed13870422f721659746d2ce797
Denis Gomes <denisgomes938@gmail.com>
2022-04-03 19:33:52 -0300

    logout finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,31 @@ import { MeusArtigosPage } from './pages/MeusArtigos';
 import { EditarArquivoPage } from './pages/EditarArquivo';
 import { NotFoundPage } from './pages/NotFound';
 import { Layout } from './components/Layout';
+import { RequireAuth } from './components/RequireAuth';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
 
-        <Route path="/" element={<Layout />}>
-          <Route index element={<ArtigosPage />} />
-          <Route path="/artigo/:id" element={<ArtigoPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ArtigosPage />} />
+        <Route path="/artigo/:id" element={<ArtigoPage />} />
+
+        <Route element={ <RequireAuth /> }>
           <Route path="/artigos" element={<MeusArtigosPage />} />
-          <Route path="/artigo/editar/:id" element={<EditarArquivoPage />} />
+          <Route path="/artigos/editar/:id" element={<EditarArquivoPage />} />
           <Route path="/artigos/novo" element={<EditarArquivoPage />} />
         </Route>
+      </Route>
 
-        <Route path="*" element={<NotFoundPage />} />
+      <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default App;
