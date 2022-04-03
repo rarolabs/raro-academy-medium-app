f521c217d570b641ede1422658046db19f20a741
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-03 18:30:07 -0300

    implementa página de Artigos do usuário

--- src/pages/MeusArtigosPage/index.tsx
+++ src/pages/MeusArtigosPage/index.tsx
@@ -1,22 +1,43 @@
 /* src/pages/MeusArtigos/index.tsx */
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 import { ArticleList } from "../../components/ArticleList";
 import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
+import { SemArtigos } from "../../components/SemArtigos";
 import { geraArtigos } from "../../stories/helpers/gerador-artigos";
 
 export const MeusArtigosPage = () => {
   const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
+  const [ control, setControl ] = useState(false)
+
+  async function buscaMeusArtigos() {
+
+    const token = localStorage.getItem("access_token")
+    
+    const response = await axios.get<ArticleThumbnailProps[]>(
+      'http://3.221.159.196:3307/artigos/meus-artigos', 
+      {
+        headers: {
+          "Authorization": `bearer ${token}`
+        }
+      }
+    );
+
+    setArticles(response.data);
+    setControl(true)
+  }
 
   useEffect(() => {
-    setArticles(
-      geraArtigos(5).map((artigo) => ({ ...artigo, editavel: true }))
-    );
+    buscaMeusArtigos();
   }, []);
 
+
+  if (control) {
+    console.log("ole")
+    return (<ArticleList articles={articles}/> ) 
+  }
   return (
-    <div className="my-30">
-      <ArticleList articles={articles} />
-    </div>
+    <div />
   );
 };
