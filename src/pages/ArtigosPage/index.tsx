7dd64d06f196f3197a5e401855ff839593d3af75
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-03 18:28:11 -0300

    implementa página de Artigos

--- src/pages/ArtigosPage/index.tsx
+++ src/pages/ArtigosPage/index.tsx
@@ -1,13 +1,34 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { ArticleList } from "../../components/ArticleList";
 import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
+import { SemArtigos } from "../../components/SemArtigos";
 import { geraArtigos } from "../../stories/helpers/gerador-artigos";
 
 export const ArtigosPage = () => {
-     const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
+    const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
+    const [ control, setControl ] = useState(false)
 
-    useEffect(() => { setArticles(geraArtigos(10)); }, []);
-    
+    useEffect(() => { buscaArtigos() }, []);
+
+    async function buscaArtigos() {
+        const token = localStorage.getItem("access_token")
+        const response = await axios.get("http://3.221.159.196:3307/artigos", {
+            headers: {
+                "Authorization": `bearer ${token}`
+            }
+        })
+
+        setArticles( response.data )
+        setControl(true)
+    }
+    console.log(control)
+
+    if (control) {
+        console.log("ole")
+        return (<ArticleList articles={articles}/> ) 
+    }
     return (
-        <ArticleList articles={articles} />
-); };
+        <div />
+    );
+};
