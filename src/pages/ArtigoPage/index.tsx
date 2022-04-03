9eb414e5154e3d2ef22117695c6ff7ef348e1a15
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-03 18:29:23 -0300

    implementa página de Artigos

--- src/pages/ArtigoPage/index.tsx
+++ src/pages/ArtigoPage/index.tsx
@@ -1,36 +1,69 @@
 /* src/pages/Artigo/index.tsx */
 import faker from "@faker-js/faker";
+import axios from "axios";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { ArticleView } from "../../components/ArticleView";
 
-export const ArtigoPage = () => {
-  const [article, setArticle] = useState<string>('');
-  const [autor] = useState({
-    nome: faker.name.firstName(),
-    avatar: faker.image.avatar(),
-  });
-  const [dataPublicacao] = useState(new Date());
+interface IArticle {
+  autor: {
+    id: number, 
+    nome: string, 
+    login: string,
+    senha: string
+    avatar: string, 
+  },
+  resumo: string, 
+  imagem: string, 
+  conteudo: string,
+}
 
-  useEffect(() => {
-    async function loadArticle() {
-      // este article.md precisa ser adicionado, temporariamente ao nosso código. Podemos copiar este conteúdo dentro da nossa pasta /public.
-      // sugiro que você retire este documento de `src/stories/assets/markdown/article.md`
-      const response = await fetch('/article.md');
-      const article = await response.text();
-      setArticle(article);
-    }
+export const ArtigoPage = () => {
+  const [ article, setArticle ] = useState< IArticle >({
+    autor: {
+      id: 0, 
+      nome: "string", 
+      login: "",
+      senha: "",
+      avatar: ""
+    },
+    resumo: "", 
+    imagem: "",
+    conteudo: "",
+  });
+  const [ autor, setAutor ] = useState({
+    nome: "",
+    avatar: ""
+  });
+  const [dataPublicacao, setDataPublicacao] = useState(new Date());
+
+  const { id } = useParams()
+
+  useEffect(() => { buscaArtigo() }, []);
+
+  async function buscaArtigo() {
+    const token = localStorage.getItem("access_token")
+    const response = await axios.get(
+      `http://3.221.159.196:3307/artigos/${id}`, {
+        headers: {
+          "Authorization": `bearer ${token}`
+        }
+      }
+    )
     
-    loadArticle();
-  }, []);
+    setArticle(response.data)   
+  }
 
   return (
+    article.conteudo !== ""  ?
     <div className="m-10">
       <ArticleView
-        article={article}
-        autor={autor}
+        article={article.conteudo}
+        autor={{nome: article.autor.nome, avatar: article.autor.avatar }}
         dataPublicacao={dataPublicacao}
         tempoLeitura={ '10min' }
       />
-    </div>
+    </div> :
+    <div></div>
   );
 };
