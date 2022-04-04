58a0bd3e61dfac183fb647b641525bc26cac0515
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-04 01:22:34 -0300

    refatora comp Article List

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -7,16 +7,17 @@ export const ArticleList: React.FC<ArticleListProps> = ({
   articles
 }) => {
   return (
-    articles.length !== 0 ? <div className="flex flex-col items-center justify-center m-10"> 
-      {
-        articles.map(article => (
-          <ArticleThumbnail
-            key={ article.titulo }
-            {...article}
-          />
-        ))
-      }
-    </div> :
-    <SemArtigos />
+    articles.length !== 0 ? 
+      <div className="flex flex-col items-center justify-center m-10"> 
+        {
+          articles.map(article => (
+            <ArticleThumbnail
+              key={ article.titulo }
+              {...article}
+            />
+          ))
+        }
+      </div> :
+      <SemArtigos />
   );
 }
