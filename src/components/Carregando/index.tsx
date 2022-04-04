d9af704c0fd0329a3662e9558900d1a402f1cb89
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-04 01:20:34 -0300

    implementa comp carregando

---
+++ src/components/Carregando/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+export function Carregando () {
+    return (
+        <div className="carregando">
+            <h1>
+                Carregando... 
+            </h1>          
+        </div>
+    )
+}
