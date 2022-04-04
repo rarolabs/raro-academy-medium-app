502975906dae66ea70e107cb804cbc9094637f31
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-04 00:00:39 -0300

    implementa estilo default comp button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -17,7 +17,11 @@ export const Button: React.FC<ButtonProps> = ({
   return (
     <button
       type={ type }
-      className={className}
+      className={ className ? className : `
+      w-full mt-6 tracking-widest
+      border-b-blue-600 bg-blue-500 py-3 text-white font-bold
+      hover:bg-blue-400 active:translate-y-[0.125rem] active:border-b-blue-400
+      ` }
       onClick={onClick}
     >
       { children }
