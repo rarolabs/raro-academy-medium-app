f981b807435d03dd2db38bda04ef147df169343e
Eduardo Borges <alemaobrk1@gmail.com>
2022-04-04 00:09:03 -0300

    altera rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './App.css';
-import { Login } from './components/Login';
-import { BrowserRouter, MemoryRouter, Route, Routes, useParams } from 'react-router-dom';
-import { Button } from './components/Button';
+import {  Route, Routes } from 'react-router-dom';
 import { ArtigosPage } from './pages/ArtigosPage'
 import { LoginPage } from './pages/LoginPage';
 import { ArtigoPage } from './pages/ArtigoPage';
@@ -9,6 +7,7 @@ import { MeusArtigosPage } from './pages/MeusArtigosPage';
 import { EditarArquivoPage } from './pages/EditarArquivoPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { Layout } from './components/Layout';
+import { RequireAuth } from './components/RequireAuth';
 
 // const Aluno = () => {
 //   const { aluno } = useParams()
@@ -20,19 +19,22 @@ import { Layout } from './components/Layout';
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
+      <Routes>
+      <Route path="/login" element={<LoginPage />} />
 
-          <Route path="/" element={<Layout/>} >
-            <Route index element={<ArtigosPage />} />
-            <Route path="/artigo/:id" element={<ArtigoPage />} />
-            <Route path="/artigos" element={<MeusArtigosPage />} />
-            <Route path="/artigos/editar/:id" element={<EditarArquivoPage />} />
-            <Route path="/artigos/novo" element={<EditarArquivoPage />} />
-          </Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ArtigosPage />} />
+        <Route path="/artigo/:id" element={<ArtigoPage />} />
 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Route element={ <RequireAuth /> }>
+          <Route path="/artigos" element={<MeusArtigosPage />} />
+          <Route path="/artigos/editar/:id" element={<EditarArquivoPage />} />
+          <Route path="/artigos/novo" element={<EditarArquivoPage />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
