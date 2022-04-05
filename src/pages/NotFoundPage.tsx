cf1d58611298876a8747a92218b3664266a33e3f
Laysa <laysa.viana@outlook.com>
2022-04-04 22:39:44 -0300

    Atualização do código

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,8 @@
 
 export const NotFoundPage = () => {
   return (
-    <p>Página não Encontrada</p>
+    <div className="flex items-center justify-center w-screen h-screen bg-indigo-600">
+      <p className="text-5xl text-white md:text-7xl lg:text-9xl">Página não encontrada</p>
+    </div>
   )
 }
