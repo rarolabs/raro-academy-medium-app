a52017d5b73f5103b54f851a057da3bcd7a15374
Laysa <laysa.viana@outlook.com>
2022-04-04 22:46:11 -0300

    Substituição do gradient

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 
 export const NotFoundPage = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-indigo-600">
+    <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
       <p className="text-5xl text-white md:text-7xl lg:text-9xl">Página não encontrada</p>
     </div>
   )
