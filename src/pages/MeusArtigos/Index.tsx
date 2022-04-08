34d1529556a1454a5154ad42673a9a63a510c7c9
Laysa <laysa.viana@outlook.com>
2022-04-08 00:11:12 -0300

    Exibir Artigos

--- src/pages/MeusArtigos/Index.tsx
+++ src/pages/MeusArtigos/Index.tsx
@@ -1,17 +1,26 @@
-
+import axios from "axios";
 import { useEffect, useState } from "react";
-
 import { ArticleList } from "../../components/ArticleList";
 import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
-import { geraArtigos } from "../../stories/helpers/gerador-artigos";
+
 
 export const MeusArtigosPage = () => {
   const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
+  const [exibirMensagem, setExibirMensagem] = useState(true);
+  const url = "http://3.221.159.196:3307/artigos/meus-artigos"
+  const chamadoNaApi = async () => {
+    const token = localStorage.getItem("access_token") ?? "";
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `bearer ${token}`
+      }
+    });
+    setArticles(response.data);
+    setExibirMensagem(response.data.length === 0);
+  }
 
   useEffect(() => {
-    setArticles(
-      geraArtigos(5).map((artigo) => ({ ...artigo, editavel: true }))
-    );
+    chamadoNaApi()
   }, []);
 
   return (
